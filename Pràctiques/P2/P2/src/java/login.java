4f8459f3c2a4a47716b12dd3103d614dc3cb6af4
Marc Casellas <marccasellas@client51-10-192-118.eduroam.upc.edu>
2018-09-21 10:15:08 +0200

    Reorder

---
+++ Pràctiques/P2/P2/src/java/login.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author marc.casellas
+ */
+@WebServlet(urlPatterns = {"/login"})
+public class login extends HttpServlet {
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet login</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet login at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        
+        String user = request.getParameter("user");
+        String pass = request.getParameter("pass");
+        Connection connection = null;
+        try {    
+    
+         Class.forName("org.sqlite.JDBC");
+   
+          // create a database connection
+          connection = DriverManager.getConnection("jdbc:sqlite:F:\\windows\\AD\\P2\\database.db");
+          Statement statement = connection.createStatement();
+          statement.setQueryTimeout(30);  // set timeout to 30 sec.
+          /**
+          
+          //S'ha de fer amb prepared statement
+          ResultSet rs = statement.executeQuery("SELECT * FROM usuaris WHERE user = '" + user + "' AND pass = '" + pass + "'");
+          
+         */
+                  
+    
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM usuaris WHERE user = ? AND pass = ?");
+        preparedStatement.setString(1, user);
+        preparedStatement.setString(2, pass);
+
+        ResultSet rs = preparedStatement.executeQuery();
+        
+        Statement stmts = (Statement) connection.createStatement();
+        
+            if(rs.next()) {
+                response.sendRedirect("menu.jsp");
+            }
+            else {
+               // Error
+               out.println("Username or Password incorrect");
+               response.sendRedirect("login.jsp");
+            }
+        }
+        catch(SQLException | ClassNotFoundException e)
+        {
+          System.err.println(e.getMessage());
+        }   
+        finally
+        {
+          try
+          {
+            if(connection != null)
+              connection.close();
+          }
+          catch(SQLException e)
+          {
+            // connection close failed.
+            System.err.println(e.getMessage());
+          }
+        }    
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
