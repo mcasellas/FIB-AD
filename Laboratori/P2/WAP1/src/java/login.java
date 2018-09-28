9de0ed40bf04aab89d186fe4eaf96b038ecbad4e
Marc Casellas <marccasellas@client50-10-192-18.eduroam.upc.edu>
2018-09-28 10:23:35 +0200

    images

--- Laboratori/P2/WAP1/src/java/login.java
+++ Laboratori/P2/WAP1/src/java/login.java
@@ -49,15 +49,16 @@ public class login extends HttpServlet {
             
             Class.forName("org.sqlite.JDBC");
             
-            //Create a database connection
+            // Connexió Sergi
             try {
                 connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\rando\\OneDrive\\Documents\\GitHub\\FIB-AD\\Laboratori\\P2\\WAP1\\Users.db");
                 Statement statement = connection.createStatement();
                 statement.setQueryTimeout(30);
             }
+            // Connexió Marc
             catch(Exception e) {
                 try{
-                connection = DriverManager.getConnection("jdbc:sqlite:F:\\windows\\FIB-AD\\Laboratori\\P2\\WAP1\\Users.db");
+                connection = DriverManager.getConnection("jdbc:sqlite://Volumes/Macintosh HD//Users/marccasellas/Documents/GitHub/FIB-AD/Laboratori/P2Users.db");
                 Statement statement = connection.createStatement();
                 statement.setQueryTimeout(30);
                 }
