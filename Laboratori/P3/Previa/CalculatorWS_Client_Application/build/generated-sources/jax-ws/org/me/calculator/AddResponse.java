ae17766e00e0c119f0d5fa13bceccf67035b2047
Marc <marc.casellas@est.fib.upc.edu>
2018-10-19 11:24:43 +0200

    BD

--- Laboratori/P3/Previa/CalculatorWS_Client_Application/build/generated-sources/jax-ws/org/me/calculator/AddResponse.java
+++ Laboratori/P3/Previa/CalculatorWS_Client_Application/build/generated-sources/jax-ws/org/me/calculator/AddResponse.java
@@ -8,9 +8,9 @@ import javax.xml.bind.annotation.XmlType;
 
 
 /**
- * <p>Clase Java para addResponse complex type.
+ * <p>Java class for addResponse complex type.
  * 
- * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
  * &lt;complexType name="addResponse">
@@ -36,7 +36,7 @@ public class AddResponse {
     protected int _return;
 
     /**
-     * Obtiene el valor de la propiedad return.
+     * Gets the value of the return property.
      * 
      */
     public int getReturn() {
@@ -44,7 +44,7 @@ public class AddResponse {
     }
 
     /**
-     * Define el valor de la propiedad return.
+     * Sets the value of the return property.
      * 
      */
     public void setReturn(int value) {
