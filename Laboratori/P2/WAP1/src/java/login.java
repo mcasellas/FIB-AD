0b249d434b94efd983a9b731dfa4cfcf97f4a26d
Marc <marc.casellas@est.fib.upc.edu>
2018-10-19 10:04:55 +0200

    Marc

--- Laboratori/P2/WAP1/src/java/login.java
+++ Laboratori/P2/WAP1/src/java/login.java
@@ -55,7 +55,8 @@ public class login extends HttpServlet {
             
             // Connexió Sergi
             try {
-                connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\rando\\OneDrive\\Documents\\GitHub\\FIB-AD\\Laboratori\\P2\\WAP1\\FotOK.db");
+                connection = DriverManager.getConnection("jdbc:derby://localhost:1527/FotOK;user=mcasellas;password=1234");
+                
                 Statement statement = connection.createStatement();
                 statement.setQueryTimeout(30);
             }
